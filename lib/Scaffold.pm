cdbf7df069e0f0b32cd519bc7d19d3383b6ba84a
Kevin L. Esteb <kesteb@wsipc.org>
2010-09-17 15:17:11 +0000

    Import of KESTEB/Scaffold-0.04 from CPAN.
    gitpan-cpan-distribution: Scaffold
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KESTEB/Scaffold-0.04.tar.gz
    gitpan-cpan-author:       KESTEB
    gitpan-cpan-maturity:     released

--- lib/Scaffold.pm
+++ lib/Scaffold.pm
@@ -4,7 +4,7 @@ use 5.8.8;
 use warnings;
 use strict;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 1; 
 
@@ -271,6 +271,7 @@ See, simple, no muss, no fuss.
  Scaffold::Handler
  Scaffold::Handler::ExtDirect
  Scaffold::Handler::ExtPoll
+ Scaffold::Handler::Default
  Scaffold::Handler::Favicon
  Scaffold::Handler::Robots
  Scaffold::Handler::Static
